f4c8fc9674c248b9cd451fba0b6656efd04058c7
Dimitris Stavroulakis <jimstav@gmail.com>
2025-04-15 21:20:18 +0300

    Simple Components

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el!);
+
+class App extends React.Component {
+  render() {
+    return <div>Hi there</div>;
+  }
+}
+
+root.render(<App />);
